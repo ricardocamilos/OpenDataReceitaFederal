962e8d24650a8ac4db92e8af0ba1341bbd4e8e23
Ricardo Camilo <ricardo.camilos@gmail.com>
2019-08-10 15:14:01 -0300

    Cria as tabelas tipo de registro e comentários
    Criação das tabelas:
    1-registro com sk
    2-registro com tipo
    3-cnpj
    4-socio
    5-cnae

--- CriaParquetRFB.sql
+++ CriaParquetRFB.sql
@@ -1,10 +1,49 @@
-use cp;
+/*Define que o schema padrao, deve ser mapeado no workspace*/
+use dfs.receita_federal;
+/*Exibe os arquivos que estão no workspace*/
 show files;
-
+/*Configura a gravacao em formato Parquet*/
 ALTER SESSION SET `store.format` = 'parquet';
 
-create table dfs.arquivo.cnpj_receita_federal_pq as
-select row_number() over(partition by 1) as rownum, columns[0] as texto from dfs.`Drill/receita_federal/SAMPLE_F.K032001K.D90308.csv`;
+/*Criei um arquivo com pouco mais de 100mil registros*/
 
-create table dfs.arquivo.cnpj_receita_federal_pq2 as
-select substr(columns[0],1,1) as fl_tipo,columns[0] as texto from dfs.`Drill/receita_federal/SAMPLE_F.K032001K.D90308.csv`;
+/*Cria uma copia dos dados em parquet com SK*/
+drop table receita_federal_pq;
+
+create table receita_federal_pq as
+select row_number() over(partition by 1) as rownum, columns[0] as texto from `SAMPLE_F.K032001K.D90308_100.csv`;
+
+select * from receita_federal_pq;
+
+/*Cria uma tabela com uma coluna de tipo de registro*/
+drop table receita_federal_reg;
+
+create table receita_federal_reg as 
+select $0 as rownum, cast(substring($1,1,1) as integer) as tp_registro,substring($1,2) as registro 
+from receita_federal_pq;
+/*Este erro estava acontecendo e foi corrigido*/
+--Unexpected byte 0xa8 at position 984804
+--Abrir arquivo no EmEditor e salvar UTF8 without Signature ou converter via unix
+
+select * from receita_federal_reg;
+
+/*Cria uma tabela com os registros das empresas*/
+drop table receita_federal_cnpj;
+create table receita_federal_cnpj as
+select registro from receita_federal_reg where tp_registro=1;
+
+select * from receita_federal_cnpj;
+
+/*Cria uma tabela com os registros dos socios*/
+drop table receita_federal_socio;
+create table receita_federal_socio as
+select registro from receita_federal_reg where tp_registro=2
+
+select * from receita_federal_socio;
+
+/*Cria uma tabela com os registros dos socios*/
+drop table receita_federal_cnae;
+create table receita_federal_cnae as
+select registro from receita_federal_reg where tp_registro=6
+
+select * from receita_federal_socio;
